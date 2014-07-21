c77d81b5eafe062805f577dee3d21076c4b16d95
Widyanto Bagus Priambodo <bobby.priambodo@gmail.com>
2014-07-22 00:23:18 +0700

    minor thumbnail fix and added screenshot

--- functions.php
+++ functions.php
@@ -43,7 +43,7 @@ function kibar_magangan_setup() {
 	 */
 	add_theme_support( 'post-thumbnails' );
 	set_post_thumbnail_size( 300, 300, true );
-	add_image_size( 'featured-latest', 9999, 300, true );
+	add_image_size( 'featured-latest', 914, 300, true );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
